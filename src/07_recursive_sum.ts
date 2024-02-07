6567b5e17c79966b484372d7aa4c888b95885fd5
Alfredo Monteiro <amonteiro.alfredo@gmail.com>
2024-02-07 16:18:55 -0100

    update recursive sum

--- src/07_recursive_sum.ts
+++ src/07_recursive_sum.ts
@@ -1,15 +1,24 @@
-function sum (n: number): number {
-  // Base Case
-  if (n === 1) return 1;
-  
-  // Recursive
-  return n + sum(n - 1);
+function sum_v1(n: number): number {
+  let sum = 0;
+  for (let i = 1; i <= n; ++i) {
+    sum += i;
+  }
+  return sum;
 }
 
 function sum_v2(n: number): number {
-  let sum = 0;
-  for (let i = 1; i <= n; ++i) {
-    sum += i
-  }
-  return sum;
+  // Base Case
+  if (n === 1) return 1;
+
+  // Recursive
+  return n + sum_v2(n - 1);
+}
+
+function sum_v3(from: number, to: number, result: number): number {
+  if (from > to) return result;
+
+  result += from;
+  console.log("I: " + from + ", sum: " + result);
+
+  return sum(from + 1, to, result);
 }
