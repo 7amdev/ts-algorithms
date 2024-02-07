93d6769b423687588c45fb772b82989d86ab962b
Alfredo Monteiro <amonteiro.alfredo@gmail.com>
2024-02-07 15:23:57 -0100

    fix for loop stop condition

--- src/03_two_crystal_ball.ts
+++ src/03_two_crystal_ball.ts
@@ -1,8 +1,8 @@
 // Given two crystal balls that will break if dropped from high enough
-// distance, determine the next spot in which it will break in the 
+// distance, determine the next spot in which it will break in the
 // most optimized way.
 
-function crystal_ball(input:boolean[]) {
+function crystal_ball(input: boolean[]) {
   // define the increase amount
   const increase_amount = Math.floor(Math.sqrt(input.length));
   let i = 0;
@@ -13,14 +13,14 @@ function crystal_ball(input:boolean[]) {
       break;
     }
   }
-  
+
   i -= increase_amount;
 
-  for (;i < input.length; ++i) {
+  for (; i <= i + increase_amount; ++i) {
     if (input[i]) {
       return i;
     }
-  } 
+  }
 
   return -1;
 }
